167d0bc3924bd6a3138cdfff421856c16d050a0b
SvKhashe <svkhashe@microsoft.com>
2020-04-04 21:41:20 +0300

    login and logout

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_session import Session
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='web')
+#app.config['SESSION_TYPE'] = 'filesystem'
+app.testing = True
+#app.secret_key = 'testing'
+app.config.update(
+            SECRET_KEY='foo',
+            SERVER_NAME='127.0.0.1:5000'
+)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,45 @@
 from app import app
-from flask import json
+from flask import json, request, redirect, send_from_directory, session
 import pyodbc
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Hello, World!"
-
-companies = [{"id": 1, "name": "Company One"}, {"id": 2, "name": "Company Two"}]
+    #print('sveta', flush=True)
+    return app.send_static_file('login.html')
 
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=SVKHASHE-T470S;'
-                      'Database=Test_tmp;'
+                      'Database=Portfolio;'
                       'Trusted_Connection=yes;')
 
-cursor = conn.cursor()
-cursor.execute('SELECT * FROM Test_tmp.dbo.Table1')
+#####################################login process#####################################
+@app.route('/login/', methods=['POST'])
+def get_login():
+    cursor = conn.cursor()
+    username = request.form.get('username')
+    pas = request.form.get('password')
+    try:
+        row = cursor.execute('SELECT id, password, name FROM Portfolio.dbo.Users where Login = \'' + str(username)+'\'').fetchone()
+    except Exception as err:
+        return '', 400
 
-arik = ''
-for row in cursor:
-    arik += str(row) + "\n"
+    if row and row.password.strip() == pas:
+        session['username'] = row.name
+        session['user_id'] = row.id
+        return redirect('http://127.0.0.1:5000/index.html')
+    else:
+        return redirect('http://127.0.0.1:5000/loginError.html')
+##################################adding id to session####################################
+@app.route('/username/', methods=['GET'])
+def username():
+    return session['username']
 
-@app.route('/companies/<id>', methods=['GET'])
-def get_xx(id):
-  cursor = conn.cursor()
-  cursor.execute('SELECT name FROM Test_tmp.dbo.Table1 where id = ' + id)
-  for row in cursor:
-    return str(row[0])
+##################################adding id to session####################################
+@app.route('/logout/', methods=['GET'])
+def logout():
+    session.clear()
+    return redirect('http://127.0.0.1:5000/login.html')
 
-@app.route('/companies', methods=['GET'])
-def get_companies():
-  return arik #json.dumps(companies)
+
 
 
