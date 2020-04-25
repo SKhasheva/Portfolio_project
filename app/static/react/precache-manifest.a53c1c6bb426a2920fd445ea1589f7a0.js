b1ff6b075c093ffb99862c1f59fe4aacc620299d
SvKhashe <svkhashe@microsoft.com>
2020-04-25 21:57:56 +0300

    move resources to app/static

---
+++ app/static/react/precache-manifest.a53c1c6bb426a2920fd445ea1589f7a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d5547f2f08840e460393f8dbd5ab6e",
+    "url": "/../app/web/arik.html"
+  },
+  {
+    "revision": "4ea45d982f1162f063a7",
+    "url": "/css/main.f8808b1c.chunk.css"
+  },
+  {
+    "revision": "5f14854ad36051c3d34c",
+    "url": "/js/2.187cd94b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js/2.187cd94b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ea45d982f1162f063a7",
+    "url": "/js/main.f47bde55.chunk.js"
+  },
+  {
+    "revision": "a3dc9eebec3267ea37f4",
+    "url": "/js/runtime-main.a9cffe6c.js"
+  }
+]);
